9f43f252c2ad3181cf8b02db06cfbba2984431d4
NicMortelliti <npmortelliti@gmail.com>
2022-05-31 16:13:20 -0700

    commit tree e17f40ab078344c3d7d7c7687cad88458204af9d

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # Pipes are "or" operators. "\"s are escape characters.
+    # "/" represent the start and end of a reg. expression
+    # "reject" method will remove blanks from returned array
+    string_array = self.split(/\.|\?|!/).reject { |s| s.empty? }
+    string_array.count
   end
 end
